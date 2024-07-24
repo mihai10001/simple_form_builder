b5b3e56c38162bc1b1742d29b6fc2b61c9fb0100
mihai10001 <rvivits@gmail.com>
2024-07-24 17:56:32 +0300

    update unions (colors, indexes)

--- src/entities/ColorsUnion.js
+++ src/entities/ColorsUnion.js
@@ -1,7 +1,7 @@
 export const ColorsUnion = [
-  { id: 1, name: 'Light blue', htmlName: 'lightblue' },
-  { id: 2, name: 'Light green', htmlName: 'lightgreen' },
-  { id: 3, name: 'Light orange', htmlName: 'lightsalmon' },
-  { id: 4, name: 'Light purple', htmlName: 'thistle' },
+  { id: 1, name: 'Light blue', htmlName: 'rgba(173, 216, 230, 0.3)' },
+  { id: 2, name: 'Light green', htmlName: 'rgba(50, 205, 50, 0.3)' },
+  { id: 3, name: 'Light orange', htmlName: 'rgba(255, 160, 122, 0.3)' },
+  { id: 4, name: 'Light purple', htmlName: 'rgba(216, 191, 216, 0.5)' },
   { id: 5, name: 'Transparent', htmlName: '' }
 ];
--- src/entities/FormComponentsUnion.js
+++ src/entities/FormComponentsUnion.js
@@ -8,34 +8,34 @@ import SliderFormComponent from '../components/form/SliderFormComponent';
 import ToggleFormComponent from '../components/form/ToggleFormComponent';
 
 export const FormComponentsUnion = [
-  { id: 1, name: 'Blank component' },
-  { id: 2, name: 'TextField component' },
-  { id: 3, name: 'Checkbox component' },
-  { id: 4, name: 'DateTime component' },
-  { id: 5, name: 'Select component' },
-  { id: 6, name: 'Button component' },
-  { id: 7, name: 'Slider component' },
-  { id: 8, name: 'Toggle component' }
+  { id: 1, name: 'TextField component' },
+  { id: 2, name: 'Checkbox component' },
+  { id: 3, name: 'DateTime component' },
+  { id: 4, name: 'Select component' },
+  { id: 5, name: 'Button component' },
+  { id: 6, name: 'Slider component' },
+  { id: 7, name: 'Toggle component' },
+  { id: 8, name: 'Blank component' },
 ];
 
 export const RenderFormComponent = (id, label, required) => {
   switch(id) {
     case 1:
-      return <BlankFormComponent label={label} required={required} />;
-    case 2:
       return <TextFieldFormComponent label={label} required={required} />;
-    case 3:
+    case 2:
       return <CheckboxFormComponent label={label} required={required} />;
-    case 4:
+    case 3:
       return <DateTimeFormComponent label={label} required={required} />;
-    case 5:
+    case 4:
       return <SelectFormComponent label={label} required={required} />;
-    case 6:
+    case 5:
       return <ButtonFormComponent label={label} required={required} />;
-    case 7:
+    case 6:
       return <SliderFormComponent label={label} required={required} />;
-    case 8:
+    case 7:
       return <ToggleFormComponent label={label} required={required} />;
+    case 8:
+      return <BlankFormComponent label={label} required={required} />;
     default:
       return null;
   }
