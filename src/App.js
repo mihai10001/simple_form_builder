0588fe9167e664c50acbaeae749164ea43722bf7
mihai10001 <rvivits@gmail.com>
2024-07-24 16:37:18 +0300

    implement drag and drop grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React from 'react';
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap
+} from 'react-grid-dnd';
+import { Container } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return <div>hello</div>;
+  const [items, setItems] = React.useState([]);
+
+  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+    if (sourceId == null || sourceIndex == null || targetIndex == null) return;
+    const result = swap(items, sourceIndex, targetIndex);
+    return setItems(result);
+  }
+
+  return (
+    <GridContextProvider onChange={onChange}>
+      <Container maxWidth='md'>
+        <GridDropZone
+          className="dropzone"
+          id="items"
+          boxesPerRow={3}
+          rowHeight={76}
+        >
+          {items.map(item => (
+            <GridItem key={item.id}>
+              <div className="gridItem">
+                <div className="gridItemContent">
+                  {item.element}
+                </div>
+              </div>
+            </GridItem>
+          ))}
+        </GridDropZone>
+      </Container>
+    </GridContextProvider>
+  );
 }
 
 export default App;
